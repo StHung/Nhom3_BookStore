4946846eb1a5bbef0317b7b68c140e1c55568515
ChauNuyen <chaunuyen2000@gmail.com>
2021-07-21 09:34:27 +0700

    BackEnd

---
+++ Nhom3_BookStore/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nhom3_BookStore.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BillManager()
+        {
+            return View();
+        }
+
+        public ActionResult BookManager()
+        {
+            return View();
+        }
+
+        public ActionResult CategoryManager()
+        {
+            return View();
+        }
+
+        public ActionResult PublisherManager()
+        {
+            return View();
+        }
+
+        public ActionResult UserManager()
+        {
+            return View();
+        }
+
+        public ActionResult UserProfile()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
